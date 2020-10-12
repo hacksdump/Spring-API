dac7b68cb59061bf55800b16777b5e91a90add08
Pratham Shakya <pshakya.kmj@gmail.com>
2020-10-13 02:40:05 +0530

    add mapping to sum two numbers

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -16,4 +16,8 @@ public class GreetingController {
         String.format(template, name));
     }
 
+    @RequestMapping("/sum")
+    public int sum(@RequestParam(value="num1") int num1, @RequestParam(value="num2") int num2) {
+        return num1 + num2;
+    }
 }
